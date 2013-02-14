b5658cd74a4e0258f1f6843bbfe2fc2b5029a493
ZAFoster <ZFoster@madisoncollege.edu>
2013-02-14 17:47:16 -0600

    Lab 4.2 Finished

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
@@ -12,7 +14,7 @@ describe "Static pages" do
     it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App")
+                        :text => "#{base_title}")
     end
 
     it "should not have a custom page title" do
@@ -30,7 +32,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => " | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
@@ -43,7 +45,7 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => " | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 
@@ -56,7 +58,7 @@ describe "Static pages" do
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      page.should have_selector('title', :text => " | Contact")
+      page.should have_selector('title', :text => "#{base_title} | Contact")
     end
   end
 end
